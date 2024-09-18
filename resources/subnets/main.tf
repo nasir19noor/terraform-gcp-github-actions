8846399b54f61cd687826038c164cfe54741db47
nasir19noor <nasir@nasir.id>
2024-09-18 22:10:29 +0700

    update

--- resources/subnets/main.tf
+++ resources/subnets/main.tf
@@ -2,16 +2,16 @@
 module "subnets" {
   source       = "../../modules/subnets"
   project_id   = "nasir-434503"
-  network_name = "jenkins"
+  network_name = "github-actions"
 
   subnets = [
     {
-      subnet_name   = "gke"
+      subnet_name   = "github-actions-gke"
       subnet_ip     = "10.21.0.0/21"
       subnet_region = "asia-southeast1"
     },
     {
-      subnet_name   = "workload"
+      subnet_name   = "github-actions-workload"
       subnet_ip     = "10.21.15.0/24"
       subnet_region = "asia-southeast1"
     }
