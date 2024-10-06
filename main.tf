833787c2ddd08ef8a8c1d46a35a5218308732298
nasir19noor <nasir@nasir.id>
2024-10-06 20:10:08 +0700

    update

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source = "./modules/vpc"
-  network_name              = "github-actions"
+  network_name              = "github-actions-2"
   auto_create_subnetworks   = false
   project_id                = "nasir-434503"
   description               = "nasir"
